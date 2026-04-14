6d504d59f92f186fd3213af8e3bd8f00624ce228
devamangupta <aman.gupta.rn06@gmail.com>
2026-04-14 22:32:39 +0530

    list game

---
+++ list.py
@@ -0,0 +1,9 @@
+l = [['c', 'a', 'm', 'p', 'u', 'x'], ['i', 's'], ['b', 'e', 's', 't'], ['c', 'h', 'a', 'n', 'n', 'e', 'l']]
+N = []
+for i in l:
+    result = ''.join(i)
+    N.append(result)
+print(N)
+line = " ".join(N)
+print(line)
+#
