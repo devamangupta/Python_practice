20565f6cbad83a5027ef6d4ba1b76d1fe72b11b7
devamangupta <aman.gupta.rn06@gmail.com>
2026-04-14 22:40:39 +0530

    list

---
+++ space.py
@@ -0,0 +1,22 @@
+"""
+Add Space between Potential Words.
+Example:
+
+Input:
+
+['campusxIs', 'bestFor', 'dataScientist']
+Output:
+
+['campusx Is', 'best For', 'data Scientist']
+"""
+l = ['campusxIs', 'bestFor', 'dataScientist']
+N = []
+for i in l:
+    result = ''
+    for j in i:
+        if j.isupper():
+            result += ' ' + j
+        else:
+            result += j
+    N.append(result)
+print(N)
