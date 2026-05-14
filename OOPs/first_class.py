ad8b437e1d8de56f96f10222f8a34efb67f1405c
devamangupta <aman.gupta.rn06@gmail.com>
2026-05-14 12:10:43 +0530

    First class of OOPS

---
+++ OOPs/first_class.py
@@ -0,0 +1,29 @@
+# Here we are going to learn objects, classes in python
+# Create a class name car
+class Car:
+    # Create a constructor
+    def __init__(self, name, model, year):
+        self.name = name
+        self.model = model
+        self.year = year
+
+    # Create a method to display the car details
+    def display(self):
+        print(f"Car Brand_Name: {self.name}")
+        print(f"Car Model: {self.model}")
+        print(f"Car Year: {self.year}")
+class Hello:
+    def __init__(self):
+        print("Hello World")
+    def display(self):
+        print("This is my first class and we try to call it using object")
+# Create an object for hello class
+greeting = Hello() #This will call the constructor of the class and print "Hello World"
+greeting.display() # This will call the display method of the class and print "This is my first class and we try to call it using object"
+
+"""
+# Create an object of the Car class
+maruti = Car("Maruti", "Swift", 2026)
+# Call the display method of the Car class
+maruti.display()
+"""
